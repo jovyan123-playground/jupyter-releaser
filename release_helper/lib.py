ffbe097830061f8d8833e5845be859e1182ac113
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2021-03-30 03:47:47 -0500

    try a windows fix

--- release_helper/lib.py
+++ release_helper/lib.py
@@ -383,11 +383,8 @@ def prep_git(branch, repo, auth, username, url):
 
     if not branch:
         # Get the default remote branch
-        info = util.run("git remote show origin")
-        for line in info.splitlines():
-            if line.strip().startswith("HEAD branch:"):
-                branch = line.strip().split()[-1]
-                break
+        ref = util.run("git symbolic-ref refs/remotes/origin/HEAD")
+        branch = ref.split("/")[-1]
 
     util.run(f"git fetch origin {branch}")
 
